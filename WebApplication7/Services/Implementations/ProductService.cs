2c608257a0a867ae16133b305b08d7c278cd6225
Mohammad Reza Rajabali <mrrajabali@gmail.com>
2025-05-24 20:43:58 +0330

    Add initial structure for ProductService and Context folder
    Introduced a new `ProductService` class in the
    `WebApplication7.Services.Implementations` namespace and an
    `IProductService` interface in the `WebApplication7.Services.Interfaces`
    namespace. Both are currently empty placeholders.
    
    Updated the `WebApplication7.csproj` file to include a new `Context`
    folder in the project structure, likely for future context-related files.

---
+++ WebApplication7/Services/Implementations/ProductService.cs
@@ -0,0 +1,8 @@
+﻿using WebApplication7.Services.Intefaces;
+
+namespace WebApplication7.Services.Implementations
+{
+    public class ProductService : IProductService
+    {
+    }
+}
---
+++ WebApplication7/Services/Intefaces/IProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication7.Services.Intefaces
+{
+    public class IProductService
+    {
+    }
+}
