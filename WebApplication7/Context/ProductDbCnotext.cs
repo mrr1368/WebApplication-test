e00eac53f3313eaca54a431aec7d66b872966d6c
Mohammad Reza Rajabali <mrrajabali@gmail.com>
2025-05-24 21:09:31 +0330

    Add ProductDbContext and update project dependencies
    Added a new `ProductDbContext` class to configure the EF Core
    database context with a SQL Server connection string. Updated
    `WebApplication7.csproj` to include EF Core SQL Server and Tools
    NuGet packages (v9.0.5) and removed a placeholder folder entry.
    Made minor formatting changes, including unnecessary `using`
    directives and a BOM in the `.csproj` file.

---
+++ WebApplication7/Context/ProductDbCnotext.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Metrics;
+using System;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebApplication7.Context
+{
+    public class ProductDbCnotext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ProductDb;Trusted_Connection=True;MultipleActiveResultSets=true;Trust Server Certificate = true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
